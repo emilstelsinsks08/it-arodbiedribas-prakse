de7dea9e8fd112fbe9accb9cb9b7b1c134f8d8fa
emilstelsinsks08 <emils.telsinsks08@gmail.com>
2025-11-30 19:35:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const supabaseUrl = "https://your-project-name.supabase.co";
-const supabaseKey = "sb_secret_M7433Fe7oxSkQrgOm6bZfg_ZoxIUEca"; // Izmantojiet savu anon key
+const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config();
+
+const supabase = createClient(process.sb_publishable_Wp55Nqkf3pgVJv70xCkq8g_bASOAqfH, process.env.sb_secret_M7433Fe7oxSkQrgOm6bZfg_ZoxIUEca);
 
 async function loadMembers() {
   try {
-    const res = await fetch(`${supabaseUrl}/rest/v1/members`, {
-      headers: {
-        apikey: supabaseKey,
-      }
-    });
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
+    const { data: { rows }, error } = await supabase.from('members').select('*');
+    if (error) {
+      console.error('Error loading members:', error);
+    } else {
+      const list = document.getElementById("member-list");
+      list.innerHTML = rows.map(m => `<li>${m.name} - ${m.status}</li>`).join("");
     }
-    const { data } = await res.json();
-    const list = document.getElementById("member-list");
-    list.innerHTML = data.rows.map(m => `<li>${m.name} - ${m.status}</li>`).join("");
   } catch (error) {
-    console.error("Error loading members:", error);
+    console.error('Error loading members:', error);
   }
 }
 
